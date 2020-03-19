0ff451656e1ac686467abfa0ea44fbad57918fd9
Mukund Kedia <mukundkedia7478@pop-os.localdomain>
2020-03-20 03:55:06 +0530

    second commit

--- blogapi/blog/urls.py
+++ blogapi/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import BlogAPIView,BlogDetail
 
 urlpatterns = [
-    path('',views.BlogContent.as_view()),
-    path('<int:pk>/',views.BlogData.as_view())
+    # path('',views.BlogContent.as_view()),
+    # path('<int:pk>/',views.BlogData.as_view())
+    path('',BlogAPIView.as_view()),
+    path('<int:id>/',BlogDetail.as_view())
 ]
--- blogapi/blog/views.py
+++ blogapi/blog/views.py
@@ -1,15 +1,49 @@
 # from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Blog
 from .serializers import BlogSerializer
+from rest_framework.views import APIView
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here
+class BlogAPIView(APIView):
+    def get(self,request):
+        blogdata = Blog.objects.all()
+        serializer = BlogSerializer(blogdata, many=True)
+        return Response(serializer.data)
 
-class BlogContent(generics.ListAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
 
+    def post(self, request):
+        data = request.data
+        serializer = BlogSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_BAD_REQUEST)        
 
-class BlogData(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
+class BlogDetail(APIView):
+    def get_object(self,id):
+        try:
+            return Blog.objects.get(id=id)
+
+        except:
+            return HttpResponse(status=status.HTTP_404_BAD_REQUEST)
+
+    def get(self,request,id):
+        blog = self.get_object(id)
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data)    
+
+    def put(self,request,id):
+        blog = self.get_object(id)
+        serializer = BlogSerializer(blog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST) 
+
+    def delete(self,request,id):
+        blog = self.get_object(id)
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)       
